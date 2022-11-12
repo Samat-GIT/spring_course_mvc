0198350cea7b8ac578aa0c497a81a7a52f87723d
Samat-GIT <seitmuratov.samat@bk.ru>
2022-11-13 01:52:48 +0600

    Initial commit

---
+++ src/main/java/com/samat/spring/course_mvc/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.samat.spring.course_mvc.service;
+
+import com.samat.spring.course_mvc.dao.EmployeeDAO;
+import com.samat.spring.course_mvc.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+
+
+}
